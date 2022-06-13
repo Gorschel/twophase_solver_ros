bb265c33a638fca52169e27e002dc332d2d11f14
gorschel <jahn-georg@web.de>
2022-06-13 21:08:50 +0200

    [LAB] 13.06. changed ros service structure. image processing now included in the server

--- scripts/solver_server.py
+++ scripts/solver_server.py
@@ -2,15 +2,15 @@
 # coding: utf-8
 
 import rospy
+from cube_scan_opencv import scan_cube
 from twophase_solver.solver import solve  # actual twophase solver algorithm
 from twophase_solver_ros.srv import Solver, SolverResponse
-from std_msgs.msg import String
 
 
 def check_solution(solStr):
     """check for errors and split movecount from solution string"""
     if solStr[len(solStr) - 2:] == 'f)':
-        print "Solution: %s" % (solStr)
+        print "\nSolution found: %s" % (solStr)
         movecount = int(solStr[solStr.find('(') + 1: len(solStr) - 2])
         solution = solStr[: len(solStr) - 6]
     else:
@@ -20,14 +20,16 @@ def check_solution(solStr):
 
 
 def handle_solve(req):
-    print "\nreceived problem: %s" % (req.defstr)
-    solStr = solve(str(req.defstr))  # twophase algorithm
-    solution, movecount = check_solution(solStr)  # check for errors
+    print "\nstarting image processing.."
+    retval, cube_def_str = scan_cube()
+    print "\nfinding solution.."
+    sol_str = solve(cube_def_str)  # twophase algorithm
+    solution, movecount = check_solution(sol_str)  # check for errors
     return SolverResponse(movecount, solution)
 
 
 if __name__ == "__main__":
-    rospy.init_node('cube_solver_server')
-    s = rospy.Service('cube_solver', Solver, handle_solve)
+    rospy.init_node('cube_scan_solver_server')
+    s = rospy.Service('cube_resolutor', Solver, handle_solve)
     print "Tables loaded. Ready to solve a cube."
     rospy.spin()
